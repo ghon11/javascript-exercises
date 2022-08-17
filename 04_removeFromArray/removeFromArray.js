44e4ad963643150fe3719d21154aa6f241fb00d5
Garry Yang <yarrygang@gmail.com>
2022-08-17 20:36:37 +0100

    completed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theElements) {
+    let redacted = [];
+    for (let value of array) {
+        let toRemove =0;
+        for (let element of theElements) {
+            if (value === element) {
+                toRemove = 1
+            }
+        }
+        if (toRemove != 1) {
+            redacted[redacted.length] = value;
+        }
+        
+    }
+    return redacted;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
