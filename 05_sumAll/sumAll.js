60851bc7d9dbf4682a05100c1841ba1d4a283462
Garry Yang <yarrygang@gmail.com>
2022-08-18 15:17:25 +0100

    Completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstValue,lastValue) {
+    sum = 0;
+    if (typeof(firstValue) != "number" || typeof(lastValue) != "number" || firstValue < 0 || lastValue < 0) {
+        return("ERROR");
+    }
+    if (firstValue < lastValue) {
+        for (let i = firstValue; i <= lastValue; i++) {
+            sum += i;
+        }
+    }
+    else if(lastValue < firstValue) {
+        for (let i = lastValue; i <= firstValue; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
-
+console.log(sumAll(-5,10));
 // Do not edit below this line
 module.exports = sumAll;
